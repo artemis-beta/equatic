316f3d0bee1ffd3ffa0899e362f4fb86855b9200
artemis-beta <KXZ186@student.bham.ac.uk>
2017-02-12 21:41:35 +0000

    Added docs and made pip package structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name                =  'equatic'                                     ,
+      version             =  '0.1.5'                                       ,
+      description         =  'Safe Equation Parser via Sympy and Numpy.'   ,
+      url                 =  'http://github.com/artemis-beta/equatic'      ,
+      author              =  'Kristian Zarebski'                           ,
+      author_email        =  'krizar312@yahoo.co.uk'                       ,
+      license             =  'MIT'                                         ,
+      packages            =  ['equatic']                                   ,
+      zip_safe            =  False                                         ,
+      tests_require       =  ['pytest']                                    ,
+      setup_requires      =  ['pytest-runner']                             ,
+      install_requires    =  [ 'numpy>=1.11.3'      ,
+                               'sympy>=1.0'         ,
+                               'matplotlib>=2.0.0'  
+                             ]
+     )
