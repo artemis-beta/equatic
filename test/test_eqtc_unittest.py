0c9ee48f3faa33b7f5419b36b7ce164be5a9e179
artemisbeta <KXZ186@student.bham.ac.uk>
2018-02-05 14:43:04 +0000

    Switched off Matplotlib for CircleCI

--- test/test_eqtc_unittest.py
+++ test/test_eqtc_unittest.py
@@ -1,54 +1,59 @@
 import unittest
 from equatic import EquationParser
-import matplotlib.pyplot as plt
 import mpmath as mpm
 import numpy as np
 import sys
 
 class TestEQTC(unittest.TestCase):
-    def test_linear(self):
+    def test_linear(self, plot=False):
         print("\nRunning Function Test: x-1\n")
         test_array = np.linspace(-10, 10, 1000)
         test_parser = EquationParser('testLinear', xarray=test_array, log='ERROR')
         test_y = test_parser.parse_equation_string('x-1')
         y = test_array - 1
-        plt.xlabel('x')
-        plt.ylabel('f(x)')
-        test_plot, = plt.plot(test_array, test_y, label='EquatIC')
-        comp_plot, = plt.plot(test_array, y, label='mpmath')
-        plt.legend(handles=[test_plot, comp_plot])
-        plt.savefig("TEST00Plot.png")
-        plt.close()
+        if plot:
+            import matplotlib.pyplot as plt
+            plt.xlabel('x')
+            plt.ylabel('f(x)')
+            test_plot, = plt.plot(test_array, test_y, label='EquatIC')
+            comp_plot, = plt.plot(test_array, y, label='mpmath')
+            plt.legend(handles=[test_plot, comp_plot])
+            plt.savefig("TEST00Plot.png")
+            plt.close()
         self.assertListEqual(test_y.round(4).tolist(), y.round(4).tolist())
 
-    def test_function_1(self):
+    def test_function_1(self, plot=False):
         print("\nRunning Function Test: sinc(x)\n")
         test_array1 = np.linspace(-10*np.pi, 10*np.pi, 1000)
         test_parser1 = EquationParser('testFunction1', xarray=test_array1, log='ERROR')
         test_y1 = test_parser1.parse_equation_string('sinc(x)')
-        plt.xlabel('x')
-        plt.ylabel('f(x)')
         y1 = np.array([float(mpm.sinc(i)) for i in test_array1])
-        test_plot, = plt.plot(test_array1, test_y1, label='EquatIC')
-        comp_plot, = plt.plot(test_array1, y1, label='mpmath')
-        plt.legend(handles=[test_plot, comp_plot])
-        plt.savefig("TEST01Plot.png")
-        plt.close()
+        if plot:
+            import matplotlib.pyplot as plt
+            plt.xlabel('x')
+            plt.ylabel('f(x)')
+            test_plot, = plt.plot(test_array1, test_y1, label='EquatIC')
+            comp_plot, = plt.plot(test_array1, y1, label='mpmath')
+            plt.legend(handles=[test_plot, comp_plot])
+            plt.savefig("TEST01Plot.png")
+            plt.close()
         self.assertListEqual(test_y1.round(4).tolist(), y1.round(4).tolist())
 
-    def test_function_2(self):
+    def test_function_2(self, plot=False):
         print("\nRunning Function Test: cos(tan(x+1)+sin(x))\n")
         test_array2 = np.linspace(-10*np.pi, 10*np.pi, 1000)
         test_parser2 = EquationParser('testFunction2', xarray=test_array2, log='ERROR')
         test_y2 = test_parser2.parse_equation_string('cos(tan(0.5*x+1)+sin(0.5*x))')
-        plt.xlabel('x')
-        plt.ylabel('f(x)')
         y2 = np.array([float(mpm.cos(mpm.tan(0.5*i+1)+mpm.sin(0.5*i))) for i in test_array2])
-        test_plot, = plt.plot(test_array2, test_y2, label='EquatIC')
-        comp_plot, = plt.plot(test_array2, y2, label='mpmath')
-        plt.legend(handles=[test_plot, comp_plot])
-        plt.savefig("TEST02Plot.png")
-        plt.close()
+        if plot:
+            import matplotlib.pyplot as plt 
+            plt.xlabel('x')
+            plt.ylabel('f(x)')
+            test_plot, = plt.plot(test_array2, test_y2, label='EquatIC')
+            comp_plot, = plt.plot(test_array2, y2, label='mpmath')
+            plt.legend(handles=[test_plot, comp_plot])
+            plt.savefig("TEST02Plot.png")
+            plt.close()
         self.assertListEqual(test_y2.round(4).tolist(), y2.round(4).tolist())
 
     def test_complexnum_case(self):
@@ -97,34 +102,38 @@ class TestEQTC(unittest.TestCase):
         y = test_parser8.parse_equation_string('reciprocal(2)')
         self.assertEqual(0.5, y)
 
-    def test_square_func(self):
+    def test_square_func(self, plot=False):
         print("\nRunning Function Test: 'x**2'")
         test_array = np.linspace(-10, 10, 1000)
         test_parser = EquationParser('testSquare', xarray=test_array, log='ERROR')
         test_y = test_parser.parse_equation_string('x**2')
         y = np.square(test_array)
-        plt.xlabel('x')
-        plt.ylabel('f(x)')
-        test_plot, = plt.plot(test_array, test_y, label='EquatIC')
-        comp_plot, = plt.plot(test_array, y, label='mpmath')
-        plt.legend(handles=[test_plot, comp_plot])
-        plt.savefig("TEST03Plot.png")
-        plt.close()
+        if plot:
+            import matplotlib.pyplot as plt 
+            plt.xlabel('x')
+            plt.ylabel('f(x)')
+            test_plot, = plt.plot(test_array, test_y, label='EquatIC')
+            comp_plot, = plt.plot(test_array, y, label='mpmath')
+            plt.legend(handles=[test_plot, comp_plot])
+            plt.savefig("TEST03Plot.png")
+            plt.close()
         self.assertListEqual(test_y.round(4).tolist(), y.round(4).tolist())
 
-    def test_tanMsin_func(self):
+    def test_tanMsin_func(self, plot=False):
         print("\nRunning Function Test: 'tan(x)-sin(x)'")
         test_array = np.linspace(-10, 10, 1000)
         test_parser = EquationParser('testSquare', xarray=test_array, log='ERROR')
         test_y = test_parser.parse_equation_string('tan(x)-sin(x)')
         y = np.tan(test_array)-np.sin(test_array)
-        plt.xlabel('x')
-        plt.ylabel('f(x)')
-        test_plot, = plt.plot(test_array, test_y, label='EquatIC')
-        comp_plot, = plt.plot(test_array, y, label='mpmath')
-        plt.legend(handles=[test_plot, comp_plot])
-        plt.savefig("TEST04Plot.png")
-        plt.close()
+        if plot:
+            import matplotlib.pyplot as plt 
+            plt.xlabel('x')
+            plt.ylabel('f(x)')
+            test_plot, = plt.plot(test_array, test_y, label='EquatIC')
+            comp_plot, = plt.plot(test_array, y, label='mpmath')
+            plt.legend(handles=[test_plot, comp_plot])
+            plt.savefig("TEST04Plot.png")
+            plt.close()
         self.assertListEqual(test_y.round(4).tolist(), y.round(4).tolist())
 
 if __name__ == '__main__':
